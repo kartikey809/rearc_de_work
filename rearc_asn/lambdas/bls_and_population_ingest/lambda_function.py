9d1238cc8778e2814a556dfe762ffd9e4beba054
kartikey809 <60400920+kartikey809@users.noreply.github.com>
2025-06-12 10:06:22 +0530

    Update lambda_function.py

--- rearc_asn/lambdas/bls_and_population_ingest/lambda_function.py
+++ rearc_asn/lambdas/bls_and_population_ingest/lambda_function.py
@@ -1,17 +1,33 @@
+import logging
 from rearc_asn.lambdas.bls_and_population_ingest.ingest import sync_files, fetch_population_data, upload_to_s3
 
-def lambda_handler(event, context):
-    print("Running BLS ingest...")
-    sync_files()
+# logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-    print("Running Population API fetch...")
+def lambda_handler(event, context):
+    logger.info("Starting BLS and Population ingest process...")
+
     try:
+        logger.info("Running BLS file sync...")
+        sync_files()
+        logger.info("BLS file sync completed.")
+    except Exception as e:
+        logger.exception("Error during BLS file sync")
+
+    try:
+        logger.info("Fetching population data...")
         data = fetch_population_data()
         if data:
+            logger.info("Population data fetched successfully. Uploading to S3...")
             upload_to_s3(data)
+            logger.info("Upload to S3 completed.")
+        else:
+            logger.warning("No population data fetched.")
     except Exception as e:
-        print("Error fetching/uploading population data:", e)
+        logger.exception("Error fetching or uploading population data")
 
+    logger.info("Ingest process completed.")
     return {
         "status": "Ingest completed"
     }
